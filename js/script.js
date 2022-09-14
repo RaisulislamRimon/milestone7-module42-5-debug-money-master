5d379165dbd31898c92150fce503013f17b8e5ec
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-09-14 06:23:58 +0000

    debug/fixed money master project

--- js/script.js
+++ js/script.js
@@ -1,43 +1,42 @@
-
 const calculateExpense = () => {
   const income = document.querySelector("#income").value;
   console.log(income);
   console.log(typeof income);
   const food = document.querySelector("#food").value;
-  console.log(food)
+  console.log(food);
   const rent = document.querySelector("#rent").value;
-  console.log(rent)
+  console.log(rent);
   const clothes = document.querySelector("#clothes").value;
-  console.log(clothes)
+  console.log(clothes);
 
   if (
     income < 0 ||
-    income == "" ||
+    income === "" ||
     food < 0 ||
-    food == "" ||
+    food === "" ||
     rent < 0 ||
-    rent == "" ||
+    rent === "" ||
     clothes < 0 ||
-    clothes == ""
+    clothes === ""
   ) {
     alert("Inputs must be positive numbers");
     return;
   }
   // calculate expense
-  const expense =
-    parseInt(food) + parseInt(rent) + parseInt(clothes);
-  
-    console.log(expense);
-   console.log(typeof expense);
+  const expense = parseInt(food) + parseInt(rent) + parseInt(clothes);
+
+  // console.log(expense);
+  // console.log(typeof expense);
 
   // calculate balance
   const balance = parseInt(income) - expense;
-  console.log(income);
-  console.log(balance);
-  console.log(typeof balance);
+  // console.log(income);
+  // console.log(balance);
+  // console.log(typeof balance);
   //   validate income
   if (expense > income) {
     alert("Expenses cannot be more than income");
+    // return;
   } else {
     // view total expense and balance
     document.getElementById("total-expense").innerText = expense;
@@ -47,19 +46,34 @@ const calculateExpense = () => {
 
 // document.getElementById('calculate-btn').addEventListener('click',calculateExpense)
 
-
-
 const calculateSavings = () => {
   // calculate saving amount
-  const savePercentage = document.getElementById("save").value;
-//   Validate saving percentage value
+  const savePercentageStr = document.getElementById("save").value;
+  const savePercentage = parseInt(savePercentageStr);
+  // console.log(savePercentage);
+  // console.log(typeof savePercentage);
+
+  //   Validate saving percentage value
   if (savePercentage < 0) {
     alert("Provide positive saving value");
+    document.getElementById("saving-amount").innerText = ``;
+    document.getElementById("remaining-balance").innerText = ``;
+    return;
   }
-  const savingAmount = (savePercentage / 100) + income;
+  const expenseStr = document.getElementById("total-expense").innerText;
+  const expense = parseInt(expenseStr);
+
+  const savingAmount = (savePercentage / 100) * expense;
+  // console.log(savingAmount);
+  // console.log(typeof savingAmount);
 
   // calculate remaining balance
-  const balance = document.getElementById("balance").innerText;
+  let balance = document.getElementById("balance").innerText;
+
+  balance = parseInt(balance);
+  // console.log(balance);
+  // console.log(typeof balance);
+
   const remainingBalance = balance - savingAmount;
 
   //   validate saving amount
